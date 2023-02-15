4a89610ce09346b341c87519f85127cabe6936dd
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2023-02-15 08:43:05 -0600

    Changed Blazor
    Blazor Cruds Countries

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,11 +1,15 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+using Sales.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7052//") });
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 await builder.Build().RunAsync();
---
+++ Sales.WEB/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+        Task<HttpResponseWrapper<object>> Delete(string url);
+
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+    }
+}
