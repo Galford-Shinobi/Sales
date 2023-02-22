9dc2dc82d9386ba5971df2c1254d2f8999684a41
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2023-02-22 11:19:01 -0600

    Changed Confirm finish page
    Finish Page

--- Sales.WEB/Repositories/IRepository.cs
+++ Sales.WEB/Repositories/IRepository.cs
@@ -11,5 +11,7 @@
         Task<HttpResponseWrapper<object>> Delete(string url);
 
         Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
     }
 }
