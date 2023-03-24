591fc24bae39fb80ec807ebf072f21fa6566cc9e
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2023-03-24 09:54:56 -0600

    Chagend
    Edit User

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -14,6 +15,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7052/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddBlazoredModal();
 builder.Services.AddAuthorizationCore();
 //builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
