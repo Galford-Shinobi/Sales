e2bfe13e972cae3fd0e94af3821bac2c7f05e3b5
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2023-03-27 09:21:04 -0600

    Changed
    Fix Pagination

--- Sales.Shared/DataBase/SalesDbContext.cs
+++ Sales.Shared/DataBase/SalesDbContext.cs
@@ -13,11 +13,18 @@ namespace Sales.Shared.DataBase
         public DbSet<City> Cities { get; set; }
         public DbSet<State> States { get; set; }
 
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("CountryId", "Name").IsUnique();
             modelBuilder.Entity<City>().HasIndex("StateId", "Name").IsUnique();
 
